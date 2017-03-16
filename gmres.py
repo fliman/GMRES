1d6f9d5a6eb5bf901df38b58dccf58adc8e756a1
Yu Wang <ywang@sprgpu951.int.cggveritas.com>
2017-03-16 18:58:07 +0800

    add

--- gmres.py
+++ gmres.py
@@ -2,19 +2,22 @@ import numpy as np
 from numpy import linalg as lin
 import matplotlib.pyplot as plt
 
-def updataQR(A):
+def oneQR(A):
 	size=A.shape
+	print size
 	if(size[1] == 1):
 		x = A[:,0]
 		y = 0.0 *x
 		y[0] = lin.norm(x)
 		u = x-y
 		v= u/lin.norm(u)
-		print v.reshape(2,1) * v.reshape(1,2)
-		Q=np.eye(2)-2.0*v.reshape(2,1)*v.reshape(1,2)
+		print v.reshape(len(x),1) * v.reshape(1,len(x))
+		Q=np.eye(len(x))-2.0*v.reshape(len(x),1)*v.reshape(1,len(x))
 
 	return Q, Q.dot(A[:,0])
 
+def updateQR(Qold, Rold, H):
+	return Qold, Rold
 
 def gmres(A, b, maxn):
 	h=np.zeros(A.shape)
@@ -31,7 +34,13 @@ def gmres(A, b, maxn):
 			v = v - h[j][n]*q[j]
 		h[n+1][n] = lin.norm(v)	
 		q[n+1] = v / h[n+1][n]
-		#backward solve
+		
+		#update QR
+		if(n == 0):
+			Q,R = oneQR(h[0:n+2,0:1])
+		else:
+			Q,R = updateQR(Q, R, h[0:n+2,0:n+1])	
+		#backward solve y	
 	return 1.0
 	
 
@@ -48,9 +57,9 @@ a[i==j-1] = -1
 
 x = np.array([1, 2, 3 ,4, 5])
 b = np.array([0, 0, 0, 0, 6])
-#gmres(a, b, 10)
+gmres(a, b, 10)
 
-test=np.array([[2.0],[1.0]])
-print updataQR(test)
+# test=np.array([[2.0],[1.0],[1.0]])
+# print updataQR(test)
 
 
